7ea00e6570534f5ab6d07f8c0f341be9364698b0
osxgirl <osxgirl@mac.com>
2021-03-05 14:36:23 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records){
+  if (records.find(record => record['result']==="W"))
+    return records.find(record => record['result']==="W")['year']
+  else
+    return undefined
+}
